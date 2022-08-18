67a746b6ce0471616dd387b91df7b7074fcc5cd6
unknown <pedrogallo9@usp.br>
2022-08-18 18:18:23 -0300

    Aula 1

---
+++ Variáveis e Operações.R
@@ -0,0 +1,87 @@
+# Aula 1: Apresentação, Interface, Variáveis e Operações
+
+### Operadores matemáticos básicos
+
+# Soma
+1 + 8
+
+# Subtração
+7 - 10
+
+# Multiplicação
+54 * 46
+
+# Divisão
+62832 / 20000
+
+### Funções matemáticas
+# help(log)
+
+# Valor absoluto
+abs(-18)
+
+# Logaritmo
+log(x = 19, base = 2)
+log(19, 2)
+log2(19)
+
+# Logaritmo natural
+log(exp(1))
+
+# Exponencial
+exp(2)
+
+# Raiz quadrada
+sqrt(2)
+
+# Seno
+sin(pi/2)
+
+# Coseno
+cos(-1)
+
+# Tangente
+tan(pi/3)
+
+### Vetor
+vetor = c(8, 4, 7, 10, 9)
+
+# Número de elementos do vetor
+length(vetor)
+
+# Seleciona o maior elemento do vetor
+max(vetor)
+
+# Seleciona o menor elemento do vetor
+min(vetor)
+
+# Retorna o menor e maior elemento do vetor
+range(vetor)
+
+### Sinal de associação
+x = 1 # x recebe ou guarda o valor 1
+
+# Podemos escrever expressões com variáveis
+y = x + 8
+
+### Classes
+
+# character - texto, ou uma cadeia de caracteres, chamada de string
+x = "Palavra"
+class(x)
+
+# numeric - números reais (decimais - separação decimal por .)
+y = 3.14
+class(y)
+
+# integer - números inteiros (usa-se L, maiúsculo, para R reconhecer valor inteiro)
+z = 5L
+class(z)
+
+# logical - expressões lógicas
+w = TRUE
+class(w)
+
+# complex - números complexos 
+t = 1 + 4i
+class(t)
